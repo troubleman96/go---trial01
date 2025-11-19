f3f35262b66511cf2428bf12d82f7379c53f5908
troubleman <itslugenge96@gmail.com>
2025-11-20 00:19:41 +0300

    lesson 2

--- main.go
+++ main.go
@@ -5,3 +5,24 @@ import "fmt"
 func main() {
 	fmt.Println("hello world")
 }
+
+//func add(a int , b int) int {
+//	return a + b
+//}
+
+//arrrayys
+//numbers := [5]int{1,2,3,4}
+
+//maps
+//person := map[string]string{
+//	"name": "John",
+//	"age": "30",
+//}
+
+//structs
+//type person struct{
+//	name string
+//	age int
+//}
+//
+//p := person{name: "John", age: 30}
